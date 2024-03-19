02fdbb220dfee73d9ffadf3013f999b01275c730
Mike69lyon <161336444+Mike69lyon@users.noreply.github.com>
2024-03-19 12:30:20 +0100

    ok test

---
+++ index.js
@@ -0,0 +1,9 @@
+import chalk from 'chalk';
+console.log(chalk.red('max'));
+console.log(chalk.blue('léo'));
+console.log(chalk.gray('diallo'));
+console.log(chalk.yellow('zack'));
+
+
+const wilders = {"fadili","loic","rostom",}
+const couleur = {"white","res","green"}
